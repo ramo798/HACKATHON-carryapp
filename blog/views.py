fe4bef6f2d1c5c821a5bc863a1d625b48259c73b
ramo <knht31i@gmail.com>
2018-08-28 01:02:04 +0900

    getから値取れるようにした

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,18 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def result(request):
-    return render(request, 'blog/result.html', {})
+    d = {
+        'c1val': request.GET.get('c1val'),
+        'c2val': request.GET.get('c2val'),
+        'c3val': request.GET.get('c3val'),
+        'c4val': request.GET.get('c4val'),
+
+    }
+    return render(request, 'blog/result.html', d)
 
 def sum(request):
    d = {
        'c1val': request.GET.get('c1val')
    }
 
-   return render(request, 'result.html', d)
+   return render(request, 'blog/result.html', d)
