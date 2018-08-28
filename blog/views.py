c1c9cb7f86b84d6072ca34c16ada1f7b959ab549
ramo <knht31i@gmail.com>
2018-08-29 00:08:47 +0900

    Initializey

--- blog/views.py
+++ blog/views.py
@@ -16,16 +16,19 @@ def result(request):
     if a > b and a > c and a > d :
         posts = Contents.objects.filter(id = 1)
         mozi = Naiyou.objects.filter(id = 1)
-        url = "{% static 'pic/curry_indian_man.png' %}"
-        #url = "static 'pic/curry_indian_man.png' "
+
     if b > a and b > c and b > d :
         posts = Contents.objects.filter(id = 2)
+        mozi = Naiyou.objects.filter(id = 2)
     if c > a and c > b and c > d :
         posts = Contents.objects.filter(id = 3)
+        mozi = Naiyou.objects.filter(id = 3)
     if d > a and d > b and d > c :
         posts = Contents.objects.filter(id = 4)
+        mozi = Naiyou.objects.filter(id = 4)
     if a+b+c+d == 0 :
         posts = Contents.objects.filter(id = 1)
+        mozi = Naiyou.objects.filter(id = 1)
 
 
 
@@ -34,10 +37,7 @@ def result(request):
     d = {
         'test': a+b+c+d,
         'posts': posts,
-        'url': url,
         'mozi': mozi,
-
-
     }
     return render(request, 'blog/result.html', d)
 
