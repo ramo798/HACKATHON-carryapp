324dc58c52c76edf32a464d02ccbe6cb03b9ad11
ramo <knht31i@gmail.com>
2018-08-28 21:03:36 +0900

    ０の時のバグ修正

--- blog/views.py
+++ blog/views.py
@@ -20,7 +20,7 @@ def result(request):
         posts = Contents.objects.filter(id = 3)
     if d > a and d > b and d > c :
         posts = Contents.objects.filter(id = 4)
-    if a+b+c+d :
+    if a+b+c+d = 0 :
         posts = Contents.objects.filter(id = 1)
 
 
